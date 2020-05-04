bbac47d2b8c000941cd1685ff5e1775f73372b98
JaeEun Lee <themislee@JaeEuns-MacBook-Pro-2.local>
2020-05-04 19:25:33 +0900

    change dir name

---
+++ FrontE/src/Home.js
@@ -0,0 +1,10 @@
+import React , {Component} from 'react';
+
+export default function Home(){
+  return (
+    <nav className = "HomeNav">
+      <h1> Welcome to PLAYLISTD</h1>
+      
+    </nav>
+  )
+}
